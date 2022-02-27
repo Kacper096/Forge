5fd4994a79c3e44e58e6209bf8aa73afc3b31b1b
Kacper096 <kacpermatuszek2@gmail.com>
2022-02-27 16:30:35 +0100

    Create ExchangeOptionsInitializerFactory

---
+++ src/Forge.MessageBroker.RabbitMQ/Exchange/ExchangeOptionsInitializerFactory.cs
@@ -0,0 +1,14 @@
+﻿using Forge.MessageBroker.RabbitMQ.Options;
+
+namespace Forge.MessageBroker.RabbitMQ.Exchange
+{
+    internal static class ExchangeOptionsInitializerFactory
+    {
+        internal static IExchangeOptionsInitializer CreateInstance(IExchangeOptionsInitializer existingInstance)
+        {
+            existingInstance ??= new ExchangeOptionsInitializer();
+            existingInstance.DeadLetterExchangeOptions = new DeadLetterExchangeOptions();
+            return existingInstance;
+        }
+    }
+}
--- src/Forge.MessageBroker.RabbitMQ/Exchange/IExchangeOptionsInitializer.cs
+++ src/Forge.MessageBroker.RabbitMQ/Exchange/IExchangeOptionsInitializer.cs
@@ -1,8 +1,11 @@
-﻿namespace Forge.MessageBroker.RabbitMQ.Exchange
+﻿using Forge.MessageBroker.RabbitMQ.Options;
+
+namespace Forge.MessageBroker.RabbitMQ.Exchange
 {
     public interface IExchangeOptionsInitializer
     {
         string Name { get; set; }
         string Type { get; set; }
+        IDeadLetterExchangeOptions DeadLetterExchangeOptions { get; set; }
     }
 }
