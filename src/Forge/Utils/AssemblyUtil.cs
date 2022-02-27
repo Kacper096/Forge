9d634dfaca072f3b6172684c2b60ddbed7c8828e
Kacper096 <kacpermatuszek2@gmail.com>
2022-02-27 16:28:59 +0100

    Create AssemblyUtil.cs

---
+++ src/Forge/Utils/AssemblyUtil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Forge.Utils
+{
+    public static class AssemblyUtil
+    {
+
+        public static Type FindNonGenericDerivedTypeFromCurrentDomain(Type interfaceType)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            return FindNonGenericDerivedType(assemblies, interfaceType);
+        }
+
+        public static IEnumerable<Type> FindGenericDerivedTypesFromCurrentDomain(Type genericInterfaceType)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            return FindGenericDerivedTypes(assemblies, genericInterfaceType);
+        }
+
+        public static Type FindNonGenericDerivedType(Assembly[] assemblies, Type interfaceType)
+        {
+            if (assemblies == null || interfaceType == null)
+            {
+                return null;
+            }
+            Type foundType = assemblies.SelectMany(assembly => assembly.GetTypes())
+                                      .FirstOrDefault(type => type.GetInterfaces().Any(iType => iType == interfaceType) && !type.IsAbstract && !type.IsInterface);
+            return foundType;
+        }
+
+        public static IEnumerable<Type> FindGenericDerivedTypes(Assembly[] assemblies, Type genericInterfaceType)
+        {
+            if (assemblies == null || genericInterfaceType == null)
+            {
+                return null;
+            }
+
+            return assemblies.SelectMany(assembly => assembly.GetTypes())
+                             .Where(t => t.GetInterfaces().Where(i => i.IsGenericType).Any(i => i.GetGenericTypeDefinition() == genericInterfaceType) && !t.IsAbstract && !t.IsInterface);
+        }
+    }
+}
