a77f3bf2c40cc33379a001cc44e300eb88e361aa
Kacper096 <kacpermatuszek2@gmail.com>
2022-02-27 16:40:53 +0100

    Add DeadLetterExchangeOptions to ExchangeOptionsInitializer

--- src/Forge.MessageBroker.RabbitMQ/Exchange/ExchangeOptionsInitializer.cs
+++ src/Forge.MessageBroker.RabbitMQ/Exchange/ExchangeOptionsInitializer.cs
@@ -1,9 +1,12 @@
-﻿namespace Forge.MessageBroker.RabbitMQ.Exchange
+﻿using Forge.MessageBroker.RabbitMQ.Options;
+
+namespace Forge.MessageBroker.RabbitMQ.Exchange
 {
     public sealed record class ExchangeOptionsInitializer : IExchangeOptionsInitializer
     {
         private const string DefaultType = "topic";
         public string Type { get; set; } = DefaultType;
         public string Name { get; set; }
+        public IDeadLetterExchangeOptions DeadLetterExchangeOptions { get; set; }
     }
 }
