1ce12da2504c1911b21eec39071a363dbd9838d7
Kacper096 <kacpermatuszek2@gmail.com>
2022-02-27 16:30:11 +0100

    Create DeadLetterExchangeOptions

---
+++ src/Forge.MessageBroker.RabbitMQ/Options/DeadLetterExchangeOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Forge.MessageBroker.RabbitMQ.Options
+{
+    public sealed record class DeadLetterExchangeOptions : IDeadLetterExchangeOptions
+    {
+        public bool Enabled { get; set; } = false;
+        public string PrefixName { get; set; }
+    }
+}
---
+++ src/Forge.MessageBroker.RabbitMQ/Options/IDeadLetterExchangeOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Forge.MessageBroker.RabbitMQ.Options
+{
+    public interface IDeadLetterExchangeOptions
+    {
+        bool Enabled { get; set; }
+        string PrefixName { get; set; }
+    }
+}
