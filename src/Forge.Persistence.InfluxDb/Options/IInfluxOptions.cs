fccbbed876c3279f4a720b25aaed3c3c13c9fc1b
Kacper096 <kacpermatuszek2@gmail.com>
2022-03-17 21:01:33 +0100

    Add InfluxOptions

---
+++ src/Forge.Persistence.InfluxDb/Options/IInfluxOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Forge.Persistence.InfluxDb.Options
+{
+    public interface IInfluxOptions
+    {
+        string[] Buckets { get; set; }
+        string Ip { get; set; }
+        bool IsHttps { get; set; }
+        string Organization { get; set; }
+        int Port { get; set; }
+        string SectionName { get; }
+        string Token { get; set; }
+
+        string GetAddress();
+    }
+}
---
+++ src/Forge.Persistence.InfluxDb/Options/InfluxOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.Persistence.InfluxDb.Options
+{
+    public sealed class InfluxOptions : IInfluxOptions
+    {
+        private const string _http = "http";
+        private const string _https = "https";
+        private const string _sectionName = "InfluxConnection";
+
+        public string SectionName => _sectionName;
+        public string Ip { get; set; }
+        public int Port { get; set; }
+        public bool IsHttps { get; set; }
+
+        public string Token { get; set; }
+        public string Organization { get; set; }
+        public string[] Buckets { get; set; }
+
+        public string GetAddress() => $"{(IsHttps ? _https : _http)}://{Ip}:{Port}";
+    }
+}
