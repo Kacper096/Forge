4aa6a9692d1702a5ef3516d6e21c2a79c49fd9ad
Kacper096 <kacpermatuszek2@gmail.com>
2022-03-17 21:21:46 +0100

    Create ExpressionExtensions.cs

---
+++ src/Forge/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace Forge.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        public static string GetMemberName(this Expression expression)
+        {
+            if (expression is MemberExpression memberExp)
+            {
+                return memberExp.Member.Name?.ToLowerInvariant();
+            }
+            return null;
+        }
+
+        public static string GetValue(this Expression expression)
+        {
+            var constantValue = (expression as ConstantExpression)?.Value as string;
+            if (!string.IsNullOrEmpty(constantValue))
+            {
+                return constantValue;
+            }
+            return Expression.Lambda(expression).Compile().DynamicInvoke() as string;
+        }
+    }
+}
