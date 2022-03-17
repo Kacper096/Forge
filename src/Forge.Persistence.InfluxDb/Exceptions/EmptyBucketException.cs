aee40ae81b4bd5447d04de3e74a73af2b09572f8
Kacper096 <kacpermatuszek2@gmail.com>
2022-03-17 21:18:37 +0100

    Create EmptyBucketException.cs

---
+++ src/Forge.Persistence.InfluxDb/Exceptions/EmptyBucketException.cs
@@ -0,0 +1,13 @@
+﻿namespace Forge.Persistence.InfluxDb.Exceptions
+{
+    public class EmptyBucketException : Exception
+    {
+        public static string Code => ExceptionCodes.EmptyBucket;
+
+        public EmptyBucketException()
+            : base("Bucket cannot be null or empty.")
+        {
+
+        }
+    }
+}
