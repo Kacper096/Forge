08d8236e52ab7c6affa9ba521cddaf81c8b23944
Kacper096 <kacpermatuszek2@gmail.com>
2022-03-17 22:55:41 +0100

    Create InfluxConnectionFactory.cs

---
+++ src/Forge.Persistence.InfluxDb/Connections/InfluxConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using Forge.Persistence.InfluxDb.Options;
+using InfluxDB.Client;
+
+namespace Forge.Persistence.InfluxDb.Connections
+{
+    internal sealed class InfluxConnectionFactory
+    {
+        private readonly IInfluxOptions _options;
+        public InfluxConnectionFactory(IInfluxOptions options)
+        {
+            _options = options;
+        }
+
+        public IInfluxConnection Create()
+        {
+            var client = InfluxDBClientFactory.Create(_options.GetAddress(), _options.Token);
+            return new InfluxConnection(client);
+        }
+    }
+}
