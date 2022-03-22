dcd1d299e051d51dd5426db1047eb3b6d1d54664
Kacper096 <kacpermatuszek2@gmail.com>
2022-03-22 21:11:27 +0100

    Missing constraint for get async

--- src/Forge.Persistence.InfluxDb/Services/IInfluxService.cs
+++ src/Forge.Persistence.InfluxDb/Services/IInfluxService.cs
@@ -6,6 +6,6 @@ namespace Forge.Persistence.InfluxDb.Services
     public interface IInfluxService
     {
         bool Add<T>(string bucket, InfluxData<T> data) where T : class;
-        Task<List<T?>> GetAsync<T>(QueryBuilder queryBuilder);
+        Task<List<T?>> GetAsync<T>(QueryBuilder queryBuilder) where T : class;
     }
 }
