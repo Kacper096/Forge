ece0046a9cebd9cfd7571fe3f3e0482d0a65b308
Kacper096 <kacpermatuszek2@gmail.com>
2022-03-26 17:25:05 +0100

    Create RConnectionFactory

---
+++ Forge.Persistence.Redis/Connections/IRConnectionFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Forge.Persistence.Redis.Connections
+{
+    internal interface IRConnectionFactory
+    {
+        RConnection Generate(string name = "redis-connection");
+    }
+}
---
+++ Forge.Persistence.Redis/Connections/RConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Forge.Persistence.Redis.Options;
+using StackExchange.Redis;
+
+namespace Forge.Persistence.Redis.Connections
+{
+    internal sealed class RConnectionFactory : IRConnectionFactory
+    {
+        private readonly IRedisOptions _options;
+
+        public RConnectionFactory(IRedisOptions options)
+        {
+            _options = options;
+        }
+
+        public RConnection Generate(string name = "redis-connection")
+        {
+            var multiplexerConnection = ConnectionMultiplexer.Connect(_options.ConnectionString, (opt) =>
+            {
+                opt.ClientName = name;
+            });
+            return new RConnection(name, multiplexerConnection);
+        }
+    }
+}
