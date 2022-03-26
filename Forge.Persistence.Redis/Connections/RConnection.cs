c513284c7bd63c1e3d8af3a6deb862ef4188285b
Kacper096 <kacpermatuszek2@gmail.com>
2022-03-26 17:24:52 +0100

    Create RConnection

---
+++ Forge.Persistence.Redis/Connections/RConnection.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+namespace Forge.Persistence.Redis.Connections
+{
+    public sealed class RConnection : IConnection
+    {
+        public string Name { get; }
+        public IConnectionMultiplexer Connection { get; }
+
+        public RConnection(string name, IConnectionMultiplexer connection)
+        {
+            Name = name;
+            Connection = connection;
+        }
+    }
+}
