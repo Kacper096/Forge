154fdbef5ed50d731596cbb6ff1dd57a6200cd13
Kacper096 <kacpermatuszek2@gmail.com>
2022-07-12 22:22:42 +0200

    Create example Forge.WebHost

---
+++ examples/WebHost/Bootstrap/MessageBrokerBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Forge.MessageBroker.RabbitMQ.Routing;
+
+namespace Forge.WebHost.Bootstrap;
+
+public static class MessageBrokerBootstrapper
+{
+    public static void ConfigurePublishMessages(IPublisherMessageDestinationProvider provider)
+    {
+
+    }
+
+    public static void ConfigureSubscribeMessages(IConsumerMessageDestinationProvider provider)
+    {
+
+    }
+}
---
+++ examples/WebHost/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Forge.Api;
+using Forge.MediatR.CQRS;
+using Forge.MessageBroker.RabbitMQ;
+using Forge.Persistence.InfluxDb;
+using Forge.Persistence.Redis;
+using Forge.WebHost.Bootstrap;
+
+namespace Forge.WebHost;
+
+public static class Startup
+{
+    public static WebApplication CreateHost(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
+        builder.WebHost.ConfigureServices(services => services.RegisterServices(configuration));
+        var app = builder.Build();
+        app.UseServices(app.Environment);
+        return app;
+    }
+
+    private static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.UseCQRS();
+        services.AddErrorHandler();
+        services.AddInfluxDb(configuration);
+        services.AddRedis(configuration);
+        services.AddMemoryCache();
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    private static IApplicationBuilder UseServices(this IApplicationBuilder builder, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            builder.UseSwagger();
+            builder.UseSwaggerUI();
+        }
+
+        builder.UseRabbitMq(MessageBrokerBootstrapper.ConfigureSubscribeMessages, MessageBrokerBootstrapper.ConfigurePublishMessages);
+        builder.UseErrorHandler();
+        builder.UseRequestLocalization();
+        //app.UseLoggingMiddleware();
+        builder.UseAuthorization();
+        return builder;
+    }
+}
