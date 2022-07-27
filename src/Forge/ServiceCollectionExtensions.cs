4d01ea5695d1fb746706766d4fa8165e5ecea75f
Kacper096 <kacpermatuszek2@gmail.com>
2022-07-27 22:53:15 +0200

    Add method to apply base forge functions.

---
+++ src/Forge/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Forge;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddForge(
+        this IServiceCollection services,
+        IConfiguration configuration, 
+        string appName = ApplicationOptions.DefaultSectionName)
+    {
+        services.Configure<ApplicationOptions>(configuration.GetSection(appName));
+        return services;
+    }
+}
