404bfca1cd0ae9a2c2b3fdc050fee10bed666d82
Kacper096 <kacpermatuszek2@gmail.com>
2022-07-28 19:43:36 +0200

    Remove brackets in ExchangeOptionsInitializer from namespace.

--- src/Forge.MessageBroker.RabbitMQ/Exchange/ExchangeOptionsInitializer.cs
+++ src/Forge.MessageBroker.RabbitMQ/Exchange/ExchangeOptionsInitializer.cs
@@ -1,19 +1,18 @@
 ﻿using Forge.MessageBroker.RabbitMQ.Options;
 using System.Reflection;
 
-namespace Forge.MessageBroker.RabbitMQ.Exchange
-{
-    public sealed record class ExchangeOptionsInitializer : IExchangeOptionsInitializer
-    {
-        private const string DefaultType = "topic";
-        public string Type { get; set; } = DefaultType;
-        public string Name { get; set; }
-        public IDeadLetterExchangeOptions DeadLetterExchangeOptions { get; set; }
+namespace Forge.MessageBroker.RabbitMQ.Exchange;
 
-        internal ExchangeOptionsInitializer(string? appName)
-        {
-            Name = string.IsNullOrWhiteSpace(appName) ? Assembly.GetExecutingAssembly().GetName().Name! : appName;
-            DeadLetterExchangeOptions = new DeadLetterExchangeOptions();
-        }
+public sealed record class ExchangeOptionsInitializer : IExchangeOptionsInitializer
+{
+    private const string DefaultType = "topic";
+    public string Type { get; set; } = DefaultType;
+    public string Name { get; set; }
+    public IDeadLetterExchangeOptions DeadLetterExchangeOptions { get; set; }
+
+    internal ExchangeOptionsInitializer(string? appName)
+    {
+        Name = string.IsNullOrWhiteSpace(appName) ? Assembly.GetExecutingAssembly().GetName().Name! : appName;
+        DeadLetterExchangeOptions = new DeadLetterExchangeOptions();
     }
 }
