ce5a3ef447d446c869bced5f266e3f80270a1edc
Kacper096 <kacpermatuszek2@gmail.com>
2022-11-24 20:45:37 +0100

    Add HubInfo

---
+++ src/Forge.SignalR/HubInfo.cs
@@ -0,0 +1,39 @@
+﻿using Forge.SignalR.Attributes;
+using System;
+using System.Reflection;
+
+namespace Forge.SignalR;
+
+public record HubInfo
+{
+    private const string DefaultGroupName = "GlobalGroup";
+    private const string DefaultDescription = "Empty";
+
+    public HubInfo(Type hubType)
+    {
+        HubType = hubType;
+        var hubAttribute = hubType.GetCustomAttribute<HubAttribute>(inherit: false);
+        Name = CreateName(hubAttribute);
+        DisplayName = CreateDisplayName(hubAttribute);
+        Group = CreateGroup(hubAttribute);
+        Description = CreateDescription(hubAttribute);
+    }
+
+    public Type HubType { get; }
+    public string Name { get; }
+    public string DisplayName { get; }
+    public string Group { get; }
+    public string Description { get; }
+
+    private string CreateName(HubAttribute? attribute)
+        => attribute?.Name ?? HubType.Name;
+
+    private string CreateDisplayName(HubAttribute? attribute)
+        => attribute?.DisplayName ?? CreateName(attribute);
+
+    private static string CreateGroup(HubAttribute? attribute)
+        => attribute?.Group ?? DefaultGroupName;
+
+    private static string CreateDescription(HubAttribute? attribute)
+        => attribute?.Description ?? DefaultDescription;
+}
