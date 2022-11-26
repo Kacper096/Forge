b7e015503418395d87adda091609cb99bf09d403
Kacper096 <kacpermatuszek2@gmail.com>
2022-11-26 20:57:20 +0100

    Add service collection extensions.

---
+++ src/Forge.SignalR/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Forge.SignalR;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddStreamingSignalR(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<StreamingOptions>(configuration.GetSection(StreamingOptions.SectionName));
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseSignalR(this IApplicationBuilder app)
+    {
+        app.UseEndpoints(endpoints => endpoints.MapHubs());
+
+        return app;
+    }
+}
--- src/Forge.SignalR/StreamingOptions.cs
+++ src/Forge.SignalR/StreamingOptions.cs
@@ -4,5 +4,7 @@ namespace Forge.SignalR;
 
 public class StreamingOptions
 {
-    public TimeSpan Interval { get; set; }
+    internal const string SectionName = "streaming-hubs";
+
+    public TimeSpan Interval { get; set; } = new TimeSpan(0, 0, 1);
 }
