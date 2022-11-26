f5e46fb50cbf547875ef676ed4c201bcde0cbca9
Kacper096 <kacpermatuszek2@gmail.com>
2022-11-26 20:19:14 +0100

    Add type extensions. Check inherit type from generic or non generic type.

---
+++ src/Forge/Extensions/TypeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Forge.Extensions;
+
+public static class TypeExtensions
+{
+    public static bool InheritsGeneric(this Type type, Type genericTypeDefinition)
+    {
+        var currentType = type;
+
+        while(currentType != null)
+        {
+            if (currentType.IsGenericType && currentType.GetGenericTypeDefinition() == genericTypeDefinition)
+                return true;
+
+            currentType = currentType.BaseType;
+        }
+
+        return false;
+    }
+
+    public static bool InheritsNonGeneric(this Type type, Type inheritType)
+    {
+        var currentType = type;
+        while (currentType != null)
+        {
+            if (currentType == inheritType)
+                return true;
+
+            currentType = currentType.BaseType;
+        }
+
+        return false;
+    }
+}
